5ab9f9da566fb2b73c61014abb631233f00bc269
Matt Newell <mnewell@skysync.com>
2019-03-19 16:03:51 -0400

    Some cleanup

--- src/DatabaseCreator/CommandLineOptions.cs
+++ src/DatabaseCreator/CommandLineOptions.cs
@@ -5,14 +5,16 @@ namespace DatabaseCreator
 	public class CommandLineOptions
 	{
 		public const string DefaultMasterSecretName = "master-database-string";
-		
-		[Option('t', "test", Required = false)]
+
+		[Option('t', "test", Required = false,
+			HelpText = "Assume requests to Kubernetes API are proxied via http://127.0.0.1:8001")]
 		public bool TestMode {
 			get;
 			set;
 		}
-		
-		[Option('c', "customer", Required = true)]
+
+		[Option('c', "customer", Required = true,
+			HelpText = "The name of the SkySync customer. Will be used for database name and login.")]
 		public string CustomerName {
 			get;
 			set;
@@ -23,7 +25,7 @@ namespace DatabaseCreator
 			get;
 			set;
 		}
-		
+
 		[Option('m', "master", Required = false, Default = DefaultMasterSecretName)]
 		public string MasterSecretName {
 			get;
